915716c01a9805711754080a4644b91cec617ca8
PotatoMaster101 <16730571+PotatoMaster101@users.noreply.github.com>
2023-02-05 12:45:40 +0800

    Updated rock tile texture and added player position change check

--- Assets/PlayerBehaviour.cs
+++ Assets/PlayerBehaviour.cs
@@ -3,17 +3,30 @@ using UnityEngine;
 public class PlayerBehaviour : MonoBehaviour
 {
     public const float MovementSpeed = 3;
+    public Vector3 PreviousPosition { get; set; }
 
     // Update is called once per frame
     void Update()
     {
+        PreviousPosition = transform.position;
+
         if (Input.GetKey("a"))
-            transform.position -= Vector3.right * MovementSpeed * Time.deltaTime;
+            transform.position += Vector3.left * MovementSpeed * Time.deltaTime;
         if (Input.GetKey("d"))
             transform.position += Vector3.right * MovementSpeed * Time.deltaTime;
         if (Input.GetKey("w"))
             transform.position += Vector3.up * MovementSpeed * Time.deltaTime;
         if (Input.GetKey("s"))
-            transform.position -= Vector3.up * MovementSpeed * Time.deltaTime;
+            transform.position += Vector3.down * MovementSpeed * Time.deltaTime;
+
+        if (transform.position != PreviousPosition)
+        {
+            SpawnTrail();
+        }
+    }
+
+    void SpawnTrail()
+    {
+        Debug.Log($"Moved: {PreviousPosition} to {transform.position}");
     }
 }
